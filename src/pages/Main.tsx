57fbb3fe35b100a321a089993b9d0e16950b39b0
Danil Shashkov <shashkovdanil@gmail.com>
2023-07-04 16:26:02 +0100

    Added link component

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,8 @@
 import { Button } from '../components/Button'
+import { Link } from '../components/Link'
+
+import phone from '../assets/phone.svg'
+import cart from '../assets/cart.svg'
 
 export function Main() {
   return (
@@ -8,6 +12,23 @@ export function Main() {
       <Button variant="blue" disabled>
         Main
       </Button>
+      <Link
+        type="a"
+        props={{ href: 'tel:+80509379992' }}
+        icon={phone}
+        variant="accent"
+      >
+        +8 050 937 99 92
+      </Link>
+      <Link
+        type="link"
+        props={{
+          to: '/cart',
+        }}
+        icon={cart}
+      >
+        Cart
+      </Link>
     </div>
   )
 }
