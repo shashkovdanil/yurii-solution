9ab4e9875f4a07b17e5c33aaefe56b7f64cd3d3e
Danil Shashkov <shashkovdanil@gmail.com>
2023-07-18 17:18:52 +0100

    Added heading component

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import { Layout } from './components/Layout'
 
 import './styles/global.css'
 import './styles/reset.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Layout>
+      <RouterProvider router={router} />
+    </Layout>
   </React.StrictMode>,
 )
